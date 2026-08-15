fae679e08f69fd1a8ffbeecc3983f6c929cd0413
Viraj chauhan <virajchauhan340@gmail.com>
2026-08-15 21:27:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-function App() {
-  return (
+import characters from './data/characters.json';
+
+function App(){
+  return(
     <div>
       <h1>Genshin Team Builder</h1>
+      <p>Loaded {characters.length} characters</p>
     </div>
   );
 }
