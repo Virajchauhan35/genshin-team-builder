bec448b83e8b50e8552340fda2e12009bc722888
Viraj chauhan <virajchauhan340@gmail.com>
2026-08-16 22:36:55 +0530

    commit

--- src/compenents/charactercard.jsx
+++ src/compenents/charactercard.jsx
@@ -7,6 +7,7 @@ function CharacterCard({ character}){
             width:"150px",
             textAlign:"center"
         }}>
+            <img src={character.icon} alt={character.name} style ={{width:"80px", height:"80px"}} />
             <h3>{character.name}</h3>
             <p>{character.element}</p>
             <p>{character.wepon}</p>
